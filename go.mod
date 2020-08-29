5aa1c2390953e3a5c0c96f82399cdb867b38c401
niqitosiq <nikitashtanko@yahoo.com>
2020-08-29 17:42:39 +0400

    REST server

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.14
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.3.0
 )
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,13 +1,17 @@
 package apiserver
 
 import (
-  "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"net/http"
+	"io"
 )
 
 // APIServer ...
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 // New ...
@@ -15,6 +19,7 @@ func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	};
 }
 
@@ -24,9 +29,12 @@ func (s *APIServer) Start() error {
 		return err
 	}
 
+	s.configureRouter();
+
 	s.logger.Info("Server Started")
 
-	return nil
+
+	return http.ListenAndServe(s.config.BindAddr, s.router);
 }
 
 
@@ -39,4 +47,14 @@ func (s *APIServer) configureLogger() error {
 	s.logger.SetLevel(level)
 
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "hello")
+	}
 }
